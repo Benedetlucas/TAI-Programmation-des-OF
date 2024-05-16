bf2c73c64524fe6edb495eb45b2b5fd33cf7b745
lablinde <122289975+lablinde@users.noreply.github.com>
2024-05-16 09:55:14 +0200

    ajout operateur php code

---
+++ operateur.php
@@ -0,0 +1,102 @@
+index.html
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./index.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
+    />
+  </head>
+  <body>
+    <div class="dashboard">
+      <header class="navigation">
+        <div class="jilora-parent">
+          <h3 class="jilora">Jilora</h3>
+          <div class="utilisateur-wrapper">
+            <div class="utilisateur">Utilisateur</div>
+          </div>
+        </div>
+        <div class="segmented-control-wrapper">
+          <div class="segmented-control">
+            <div class="bigpicture">Bigpicture</div>
+            <div class="item-1">
+              <div class="item-2">
+                <div class="dtails">Détails</div>
+              </div>
+            </div>
+            <div class="item-3">
+              <div class="archives">Archives</div>
+            </div>
+            <div class="item-4">
+              <div class="popular">Popular</div>
+            </div>
+            <div class="item-5">
+              <div class="new-releases">New Releases</div>
+            </div>
+          </div>
+        </div>
+        <div class="navigation-inner">
+          <div class="frame-parent">
+            <div class="button-parent">
+              <button class="button">
+                <img
+                  class="more-horizontal-icon"
+                  alt=""
+                  src="./public/morehorizontal.svg"
+                />
+              </button>
+              <button class="button1">
+                <div class="log-out">Log out</div>
+              </button>
+            </div>
+            <div class="frame-wrapper">
+              <div class="avatar-parent">
+                <img class="avatar-icon" alt="" src="./public/avatar@2x.png" />
+
+                <div class="chevron-down-wrapper">
+                  <img
+                    class="chevron-down-icon"
+                    alt=""
+                    src="./public/chevrondown.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div class="search">
+        <img class="search-icon" alt="" src="./public/search.svg" />
+
+        <div class="label">Search...</div>
+      </div>
+      <main class="bar-chart-wrapper">
+        <section class="bar-chart">
+          <div class="of-2-parent">
+            <div class="of-2">OF 2</div>
+            <div class="assign-danae-mark-wrapper">
+              <div class="assign">Assigné à : Danae Mark</div>
+            </div>
+          </div>
+          <div class="description-container">
+            <p class="description">
+              <span class="description1">Description</span>
+              <span>
+                :
+                ..............................................................................................................................................................................................</span
+              >
+            </p>
+            <p class="p">
+              .........................................................................................................................................................................................................................................................................................................................................................................................................
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  </body>
+</html>
