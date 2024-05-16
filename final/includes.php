fc816ebec564872dd50173ae96bb4a12a4524f06
hippolyte <hippolyte.retiere@etu.estia.fr>
2024-05-16 11:23:48 +0200

    refonte de la page admin php

---
+++ final/includes.php
@@ -0,0 +1,25 @@
+<?php
+    function call_header(){
+        <?php
+
+        <header class="header">
+            <div class="logo">
+                <h1>Jilora</h1>
+            </div>
+            <nav class="menu">
+                <ul>
+                    <li><a href="#">BigPicture</a></li>
+                    <li><a href="#">Détails</a></li>
+                    <li><a href="#">Archives</a></li>
+                </ul>
+            </nav>
+            <div class="logout">
+                <button>Log out</button>
+            </div>
+        </header>
+        
+        </body>
+        </html>
+    ?>
+    }
+?>
